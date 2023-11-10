77005b68dab1850a45ed355d637d795859de3f33
shubham <ska10@iitbbs.ac.in>
2023-11-10 22:36:59 +0530

    concept of conditional rendering , shimmer js done

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,7 +2,7 @@ import RestaurantCard from "./RestaurantCard";
 import resList from "../Utils/mockdata";
 // default export is imported like this
 import { useState , useEffect } from "react";
-
+import Shimmer from "./shimmer";
 // const dummy = ()=>{
 //    console.log("button clicked form dummy");
 //    console.log(restData) ;
@@ -19,7 +19,7 @@ import { useState , useEffect } from "react";
 // super powerful react variable - also called state variable
 
 const Body = ()=>{
-   let [ restData , setrestData] = useState(resList); 
+   let [ restData , setrestData] = useState([]); 
 
    // this can also be declared as - 
    // const arr = useState(restData);
@@ -33,16 +33,13 @@ console.log(restData)
 const dummy = ()=>{
    console.log("button clicked form dummy");
    console.log(restData) ;
-   restData = restData.filter( (res) => res.rating > 4 );
+   restData = restData.filter( (res) => res.id < 10 );
 
    console.log(restData) ;
    setrestData(restData)
 }
 
-const getAll = ()=>{
-   restData = resList;
-   setrestData(restData);
-}
+
     // first way using for loop
  //    array = [];
  //    for(var i =0 ;i<resList.length ;i++){
@@ -52,25 +49,36 @@ const getAll = ()=>{
       // console.log("useEffect cb called");
        fetchData();
    } , []);
-
+   var mid;
    const fetchData = async()=>{
       
       const res = await fetch("https://jsonplaceholder.typicode.com/todos/");
-      const mid = await res.json();
-      console.log(mid)
+      mid = await res.json();
+      // console.log(mid);
+      setrestData(mid)
    }
-   
-   console.log("before use effect")
+
+   const getAll = ()=>{
+      restData = mid;
+      setrestData(restData);
+   }
+   console.log("before use effect");
+
+
+   // this is conditional rendering concept - important for interview perspective
+   if(restData.length == 0){
+      return <Shimmer />
+   }
     return (
        <div className="body">
-          <div className="filter"> <button className="filter-btn" onClick={dummy} onMouseOver={()=> console.log("on btn")}>Top Rated Restaurants</button> </div>
+          <div className="filter"> <button className="filter-btn" onClick={dummy} >Top Rated Restaurants 1</button> </div>
           <div className="filter"> <button className="filter-btn" onClick={()=>{
-            restData = restData.filter( (res) => res.rating > 4 );
+            restData = restData.filter( (res) => res.id < 10 );
             setrestData(restData);
           }}>Top Rated Restaurants</button>
          </div>
-         <div className="filter"> <button className="filter-btn" onClick={getAll}>All Restaurants</button> 
-         </div>
+         {/* <div className="filter"> <button className="filter-btn" onClick={getAll}>All Restaurants</button> 
+         </div> */}
           <div className="res-container">
              {/* {RestaurantCard()}
              <RestaurantCard resData={resList[0]}></RestaurantCard>
@@ -78,8 +86,8 @@ const getAll = ()=>{
              <RestaurantCard resData={resList[2]}></RestaurantCard> */}
              {/* second way for rendering data using map */}
              {
-                restData.map((res)=>{
-                   return <RestaurantCard resData={res}  key={res.name}/>
+                restData.map((res , index)=>{
+                   return <RestaurantCard resData={res}  key={index}/>
                 })
              }
              {/* always use key and do not use use index as key , important for react optimisation  */}
