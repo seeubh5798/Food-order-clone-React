68090951e64fff3242c7aeaa5dc1c54e0c18c018
shubham <ska10@iitbbs.ac.in>
2023-11-10 22:49:41 +0530

    login logout dynamic changes done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,10 @@
 import { HeaderLogoUrl } from "../Utils/constants";
-
+import { useState } from "react";
 const Header = ()=>{
+// let btn = "Login";
+
+   console.log("header rendered");
+   let [btn , setbtn] = useState("LogIn")
     return (
        <div className="header">
           <div className="logo-container">
@@ -13,6 +17,12 @@ const Header = ()=>{
              <li> About Us </li>
              <li> Contact Us </li>
              <li> Cart </li>
+             <li> <button className="btn-login" onClick={
+               ()=>{
+                 btn =  btn === "LogIn" ? "Log Out" : "LogIn";
+                  setbtn(btn);        // as soon as state variable changes react re renders the component itself.
+               }
+             }>{btn}</button></li>
           </ul>
          </div>
  
