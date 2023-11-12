1a0709e6eb9136a0c0e28436411aea6b0a6a0a78
shubham <ska10@iitbbs.ac.in>
2023-11-12 20:33:41 +0530

    contact router done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header"
 import Body from "./components/Body";
 import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./Error";
 // because of this complexity for simple html rendering , JSX came to the humanity.
 import { createBrowserRouter , RouterProvider } from "react-router-dom";
 
@@ -19,8 +21,10 @@ const AppLayout = ()=>{
 
 // creating routing configuration - configuration means defining some info on what will happen on that specific routes
 const routes = createBrowserRouter([
-  {path : "/" , element : <AppLayout />},
+  {path : "/" , element : <AppLayout /> , errorElement : <Error />},
   {path : "/about" , element : <About />},
+  {path : "/contact" , element : <Contact />},
+  // {path : "*" , element : <Contact />},
 ])
 
 
---
+++ src/components/Contact.js
@@ -0,0 +1,9 @@
+
+
+const Contact = ()=>{
+    return (
+        <h1> Hi! Please contact me </h1>
+    )
+}
+
+export default Contact;
