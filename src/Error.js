6d21351aa0258d5c832a8911f006b034b8a1f7b8
shubham <ska10@iitbbs.ac.in>
2023-11-12 20:34:31 +0530

    useRouteError hook use given by react-router-dom

---
+++ src/Error.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useRouteError } from 'react-router-dom'
+const Error = () => {
+    const err = useRouteError();
+  return (
+   
+    <div>
+        OOPs ! Error Occured
+        <h1> {err.data}</h1>
+        
+    </div>
+  )
+}
+
+export default Error
