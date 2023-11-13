77b0fbbc3519f9983dbbdc494d865e682dd5670d
shubham <ska10@iitbbs.ac.in>
2023-11-13 21:51:48 +0530

    react life cycle, component didmount done

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,40 @@
 import UserClass from "./UserClass";
+import React from "react";
 
-const About = ()=>{
-    return (
-        <>
-        <h1> This is about us</h1>
-        <UserClass name="Shubham Kumar Agrawal" grad="IIT Grad"/>
-        </>
-        
-    )
+class About extends React.Component {
+
+        constructor(props){
+            super(props)
+            console.log("parent constructor")
+        }
+
+
+        componentDidMount(){
+            console.log("parent componentdidmount called")
+        }
+    render(){
+        console.log("parent component")
+        return (
+                    <>
+                    {/* the below 4 will help understand react component life cycle in a better way - render phase and commit phase */}
+                    <h1> This is about us</h1>
+                    <UserClass name="Shubham" grad="IIT Grad"/>
+                    <UserClass name="Priyal" grad="NIT Grad"/>
+                    <UserClass name="Shakhi" grad="NIT-A Grad"/>
+                    <UserClass name="Priya" grad="IIT-B Grad"/>
+                    </>
+                    
+                )
+    }
 }
+// const About = ()=>{
+//     return (
+//         <>
+//         <h1> This is about us</h1>
+//         <UserClass name="Shubham Kumar Agrawal" grad="IIT Grad"/>
+//         </>
+        
+//     )
+// }
 
 export default About;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -8,17 +8,38 @@ class UserClass extends React.Component {
 
             this.state = {
                 school : "Mechanical Sciences",
-                gradYear : 2021
+                gradYear : 2021,
+                count : 0
             }
+
+            console.log("child constructor called from Userrclass" , this.props.name)
         }
 
+        componentDidMount(){
+            console.log("child componentdidmount called" , this.props.name)
+        }
+
+
         render(){
+            console.log("child render is called from userclass" , this.props.name)
             return (
                 <div className="userclass">
+                    <button onClick={()=>{
+                        // this.state.count++;  // we cannot do like this in updating state variable directly 
+                        // use this.setState from React in class component
+
+                        this.setState({
+                            count : this.state.count +1
+                        })
+                        console.log(this.state.count);
+                    }}>
+                        Increase Count
+                    </button>
                     <h1>Name - {this.props.name}</h1>
                     <h4>College - {this.props.grad}</h4>
                     <h4>Schoool - {this.state.school}</h4>
                     <h4>Year of Graduation - {this.state.gradYear}</h4>
+                    <h1>Visitors - {this.state.count}</h1>
                 </div>
             )
         }
