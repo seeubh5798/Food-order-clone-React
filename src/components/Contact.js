ecfac126aeb001bc99bdf6ed2497f6284a7fdb0f
shubham <ska10@iitbbs.ac.in>
2023-11-16 23:45:53 +0530

    internet off feature added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,14 @@
-
+import useOnlineStatus from "../Utils/useOnlineStatus";
 
 const Contact = ()=>{
+
+    const status = useOnlineStatus();
+    if(status === false){
+        return (
+            <div><h3>You are offline , pls check internet connection</h3></div>
+        )
+    }
+
     return (
         <h1> Hi! Please contact me </h1>
     )
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,10 +1,18 @@
 import { ResLogoUrl} from "../Utils/constants";
+import useOnlineStatus from "../Utils/useOnlineStatus";
 // named export is imported like this
 
 const RestaurantCard = (props)=>{
     // console.log(props);
     const { resData} = props;
     const { completed , id , title, userId } = resData;
+
+    const status = useOnlineStatus();
+    if(status === false){
+        return (
+            <div><h3>You are offline , pls check internet connection</h3></div>
+        )
+    }
     return (
        <div className="res-card" id={id}>
            <img className="res-logo" src={ResLogoUrl} />
